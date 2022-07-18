a5c17582efc00d6b09a578456e73cb2cd34e5b96
Eugene <Eugene.Nisnevich@Gmail.com>
2022-07-17 23:04:20 -0400

    Add project files.

---
+++ SHOPPING_LIST_LAB/Program.cs
@@ -0,0 +1,61 @@
+﻿decimal cartTotal = 0.00m;
+ConsoleKeyInfo key;
+var cartItems = new List<KeyValuePair<string, decimal>>();
+
+string input = "";
+Dictionary<string, decimal> menu = new Dictionary<string, decimal>();
+menu.Add("item1", 3.50m);
+menu.Add("item2", 0.99m);
+menu.Add("item3", 5.99m);
+menu.Add("item4", 9.99m);
+menu.Add("item5", 2.55m);
+menu.Add("item6", 1.99m);
+menu.Add("item7", 0.75m);
+menu.Add("item8", 0.25m);
+
+Console.WriteLine("Here is what we have on our menu:\n");
+do
+{
+    DisplayMenu();
+    Console.WriteLine("\nEnter your order");
+    input = Console.ReadLine();
+    Order(input);
+    Console.WriteLine("\nPress ENTER to add to your order or any other key to checkout");
+    key = Console.ReadKey();
+}
+while(key.Key == ConsoleKey.Enter);
+Checkout();
+
+// functions
+
+void Order(string input2)
+{
+    while (menu.ContainsKey(input) != true)
+    {
+        Console.WriteLine("\nSorry, that item is not on our menu. Please try again.");
+        input = Console.ReadLine();
+    }
+
+    Console.WriteLine("\nAdding " + input + " to your cart at $" + menu[input]);
+    cartItems.Add(new KeyValuePair<string, decimal>(input, menu[input]));    
+}
+
+void DisplayMenu()
+{
+    foreach (KeyValuePair<string, decimal> item in menu)
+    {
+        Console.WriteLine("{0}: $ {1}", item.Key, item.Value);
+    }
+}
+void Checkout()
+{
+    Console.WriteLine("Thanks for your order!\n");
+    Console.WriteLine("\nHere is your receipt:");
+    foreach (var x in cartItems)
+    {
+        Console.WriteLine(x);
+        cartTotal += x.Value;
+
+    }
+    Console.WriteLine("Your total is: $" + cartTotal);
+}
---
+++ test/Program.cs
@@ -0,0 +1,8 @@
+﻿Dictionary<string, int> dict = new Dictionary<string, int>() {
+            {"A", 1}, {"B", 2}, {"C", 3}, {"D", 4}
+        };
+
+foreach (var kvp in dict)
+{
+    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+}
